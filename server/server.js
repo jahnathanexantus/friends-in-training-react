5951c937b6b5113cb9d1d5fd7c1558a5d523a515
jahnathan x <jahnathanexantus@gmail.com>
2024-05-19 09:56:50 -0400

    initialize the socketio and logic into a separate file to keep everything organize inside the server.js file

--- server/server.js
+++ server/server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const session = require("express-session");
 const routes = require("./routes");
 const cors = require("cors");
+const initializeSocket = require("../server/utils/socket");
+const http = require("http");
 // const authenticateToken = require("../server/utils/jwt");
 const app = express();
-const http = require("http").Server(app);
+const server = http.createServer(app);
+const io = initializeSocket(server);
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -29,38 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
-
 app.use(routes);
-
-const socketIo = require("socket.io")(http, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
-socketIo.on("connection", (socket) => {
-  console.log(`⚡: ${socket.id} user just connected!`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.io(data.room).emit("receive_message", data);
-  });
-  socket.on("disconnect", () => {
-    console.log("🔥: A user disconnected");
-  });
-});
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
+app.use(initializeSocket);
 
 app.get("*", function (req, res) {
   res.status(404).send("404 - Page not found, please try again.");
 });
 
 sequelize.sync({ force: false }).then(() => {
-  http.listen(PORT, () => console.log("Now listening"));
+  server.listen(PORT, () => console.log("Now listening"));
 });
---
+++ server/utils/socket.js
@@ -0,0 +1,30 @@
+// socket.js
+const socketIo = require("socket.io");
+const express = require("express");
+const app = express();
+
+const initializeSocket = (http) => {
+  const socketIo = require("socket.io")(http, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+  socketIo.on("connection", (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+    });
+
+    socket.on("send_message", (data) => {
+      socketIo.to(data.room).emit("receive_message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("🔥: A user disconnected");
+    });
+  });
+};
+
+module.exports = initializeSocket;
