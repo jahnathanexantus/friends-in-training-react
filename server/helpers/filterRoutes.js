f0f5e2c1ff728e7ca74acba4329aa7a65e8d63d2
jahnathan x <jahnathanexantus@gmail.com>
2024-05-28 23:08:02 -0400

    saved potential functions for the helpers file

--- server/helpers/filterRoutes.js
+++ server/helpers/filterRoutes.js
@@ -57,3 +57,77 @@ async function filterUser(req) {
 }
 
 module.exports = filterUser;
+// const User = require("../models/User");
+
+// async function filterUser(req) {
+//   try {
+//     // Get the current user from the session
+//     const currentUser = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ["password"] },
+//     });
+
+//     if (!currentUser) {
+//       throw new Error("Current user not found");
+//     }
+
+//     // Get all users in the same city
+//     const filteredUsers = await User.findAll({
+//       where: { city: currentUser.city },
+//       attributes: { exclude: ["password"] },
+//     });
+
+//     return filteredUsers;
+//   } catch (err) {
+//     console.error(err);
+//     throw err;
+//   }
+// }
+
+// module.exports = filterUser;
+// const { Op } = require("sequelize");
+// const User = require("../models/User");
+
+// async function filterUser(req) {
+//   try {
+//     // Get the current user from the session
+//     const currentUser = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ["password"] },
+//     });
+
+//     if (!currentUser) {
+//       throw new Error("Current user not found");
+//     }
+
+//     // Build the where clause dynamically
+//     const whereClause = {
+//       city: currentUser.city,
+//     };
+
+//     if (currentUser.fitness_level) {
+//       whereClause.fitness_level = currentUser.fitness_level;
+//     }
+
+//     if (currentUser.availability) {
+//       whereClause.availability = currentUser.availability;
+//     }
+
+//     // Get all users matching the criteria
+//     const filteredUsers = await User.findAll({
+//       where: {
+//         ...whereClause,
+//         [Op.or]: [
+//           { fitness_level: currentUser.fitness_level },
+//           { availability: currentUser.availability },
+//         ],
+//       },
+//       attributes: { exclude: ["password"] },
+//     });
+
+//     return filteredUsers;
+//   } catch (err) {
+//     console.error(err);
+//     throw err;
+//   }
+// }
+
+// module.exports = filterUser;
