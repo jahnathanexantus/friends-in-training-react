63b95f80eee2e86012eea164ff7bb53b5d05e8d8
jahnathan x <jahnathanexantus@gmail.com>
2024-06-24 23:33:38 -0400

    created webpack for the front end

---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    filename: "client.bundle.js",
+    path: path.resolve(__dirname, "dist/client"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist/client"),
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
